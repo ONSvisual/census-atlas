04156df6289a0ed25a4c86c492f0194480b300d1
Vivian Allen <vivian.allen@methods.co.uk>
2021-11-09 08:27:25 +0000

    tweak radio-clicker script

--- performance-testing/crawl-atlas-protoype-load-all-indicators.js
+++ performance-testing/crawl-atlas-protoype-load-all-indicators.js
@@ -1,13 +1,12 @@
 // click on all unexpanded drop-down span elements for indicates. Loop n times with 1 second timeout between
 // each loop to ensure all are expanded.
-var unexpanded_dropdowns;
-var n_loops=10;
-for (i=1; i<=n_loops+1; i++) {
-  if (i<=n_loops) {
+const n_menu_expansion_loops=10;
+const n_radios_to_click=1;
+for (i=1; i<=n_menu_expansion_loops+1; i++) {
+  if (i<=n_menu_expansion_loops) {
     setTimeout(
       () => {
-        unexpanded_dropdowns = document.querySelectorAll('span.svelte-rq9fus:not(.expanded)');
-        unexpanded_dropdowns.forEach(dropdown => dropdown.click());
+        document.querySelectorAll('span.svelte-rq9fus:not(.expanded)').forEach(dropdown => dropdown.click());
       },
       i*1000
     )
@@ -19,6 +18,7 @@ for (i=1; i<=n_loops+1; i++) {
         radios = document.querySelectorAll('input.svelte-1vonfiw[type=radio]');
         // click on all radios to load indicators, with 2 second timeout between each to allow load
         for (let i=0; i<radios.length; i++) {
+          if (i>=n_radios_to_click){break}
           setTimeout(
             () => {
               radios[i].click()
