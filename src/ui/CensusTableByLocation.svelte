907d4cd8ed778f496f12d57c5f9c8a4a9c735fd6
Valentina <vmilitaru28@gmail.com>
2021-11-10 23:59:50 +0000

    adds the censusTableByLocation

---
+++ src/ui/CensusTableByLocation.svelte
@@ -0,0 +1,41 @@
+<script>
+  export let tableName;
+</script>
+
+<table class="ons-table">
+  <thead class="ons-table__head">
+    <tr class="ons-table__row">
+      <th scope="col" class="ons-table__header">
+        <span>{tableName}</span>
+      </th>
+      <th scope="col" class="ons-table__header ons-table__header--numeric">
+        <span>People</span>
+      </th>
+      <th scope="col" class="ons-table__header ons-table__header--numeric">
+        <span>Percentage</span>
+      </th>
+    </tr>
+  </thead>
+  <tbody class="ons-table__body">
+    <tr class="ons-table__row">
+      <td class="ons-table__cell "> England </td>
+      <td class="ons-table__cell  ons-table__cell--numeric"> 67,081,000 </td>
+      <td class="ons-table__cell  ons-table__cell--numeric"> 0.43 </td>
+    </tr>
+    <tr class="ons-table__row">
+      <td class="ons-table__cell "> Northern Ireland </td>
+      <td class="ons-table__cell  ons-table__cell--numeric"> 1,896,000 </td>
+      <td class="ons-table__cell  ons-table__cell--numeric"> 0.10 </td>
+    </tr>
+    <tr class="ons-table__row">
+      <td class="ons-table__cell "> Scotland </td>
+      <td class="ons-table__cell  ons-table__cell--numeric"> 5,466,000 </td>
+      <td class="ons-table__cell  ons-table__cell--numeric"> 0.05 </td>
+    </tr>
+    <tr class="ons-table__row">
+      <td class="ons-table__cell "> Wales </td>
+      <td class="ons-table__cell  ons-table__cell--numeric"> 3,170,000 </td>
+      <td class="ons-table__cell  ons-table__cell--numeric"> 0.53 </td>
+    </tr>
+  </tbody>
+</table>
