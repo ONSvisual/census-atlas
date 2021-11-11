988f56ce87f7574869bd935cb8be2c38cfeb53c7
Michał Ordon <michal@tehidea.com>
2021-11-11 02:05:34 +0000

    Clean up routes

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,8 @@
 <script>
   import CensusAtlas from "./routes/CensusAtlas.svelte";
-  import CensusAtlasV2 from "./routes/CensusAtlasV2.svelte";
+  import ExploreCensus from "./routes/ExploreCensus.svelte";
   import Components from "./routes/Components.svelte";
   import DesignSystemApp from "./routes/DesignSystemApp.svelte";
-  import SelectAreaOrVariable from "./routes/SelectAreaOrVariable.svelte";
   import ByLocationAndCategory from "./routes/ByLocationAndCategory.svelte";
   import ByLocation from "./routes/ByLocation.svelte";
   import ByCategory from "./routes/ByCategory.svelte";
@@ -14,21 +13,20 @@
 
 <Router {url}>
   <div>
-    <Route path="/" component={CensusAtlas} />
-    <Route path="/census-atlas" component={CensusAtlasV2} />
+    <Route path="/" component={ExploreCensus} />
     <Route path="/components" component={Components} />
     <Route path="/design-system" component={DesignSystemApp} />
-    <Route path="/select-area" component={SelectAreaOrVariable} />
     <Route path="/data/:categoryId/locations/:locationId" component={ByLocationAndCategory} />
     <Route path="/data/:categoryId" component={ByCategory} />
     <Route path="/locations/:locationId" component={ByLocation} />
+    <Route path="/legacy" component={CensusAtlas} />
   </div>
   <nav style="position: relative; z-index: 99999">
-    <Link to="/">Census Atlas (Legacy)</Link>
-    <Link to="/census-atlas">Census Atlas (V2)</Link>
     <Link to="/components">Components</Link>
     <Link to="/design-system">Design System</Link>
-    <Link to="/select-area">(Step 1) Select Area or Variable</Link>
-    <Link to="/category">(Step 1) Select Area or Variable</Link>
+    <Link to="/">Explore Census</Link>
+    <Link to="/data/Health">Explore by Topic (Health)</Link>
+    <Link to="/locations/Walsall">Explore by Location (Walsall)</Link>
+    <Link to="/legacy">Legacy version</Link>
   </nav>
 </Router>
