c0a4ab7a5afae289d6f45cb19a26922ad5cc1e46
Michał Ordon <michal@tehidea.com>
2021-11-11 10:52:22 +0000

    Fill up ByCategory route

--- src/routes/ByCategory.svelte
+++ src/routes/ByCategory.svelte
@@ -2,14 +2,19 @@
   import BasePage from "../ui/BasePage.svelte";
 
   import Map from "../ui/Map.svelte";
-  import ExploreByTopic from "../ui/ExploreByTopic.svelte";
-  import ExploreByAreaComponent from "../ui/ExploreByAreaComponent.svelte";
+  import Topic from "../ui/Topic.svelte";
   import ONSShare from "../ui/ons/ONSShare.svelte";
   import Feedback from "../ui/Feedback.svelte";
 
   import ONSPhaseBanner from "../ui/ons/ONSPhaseBanner.svelte";
 
   export let categoryId;
+
+  let topicList1 = [
+    { title: "How does general health differ across England and Wales?", href: "#" },
+    { title: "Which areas are home to people who provide the most unpaid care?", href: "#" },
+    { title: "What can we learn about long-term health issues and/or disability?", href: "#" },
+  ];
 </script>
 
 <svelte:head>
@@ -34,11 +39,13 @@
     </footer>
   </span>
 
-  <ExploreByTopic />
-  <hr />
-  <ExploreByAreaComponent>Search for an area to find out how it compares to others</ExploreByAreaComponent>
+  <Topic topicList={topicList1} cardTitle="Health - Census 2021"
+    >The 2021 Census tells us a lot about the health of people living in England and Wales live and. <a href="#">
+      Choose a data option from the full list</a
+    > or explore one of these suggestions.
+  </Topic>
 
-  <ONSShare />
+  <ONSShare url="https://www.google.com/">Share this page</ONSShare>
 </BasePage>
 
 <style lang="scss" global>
