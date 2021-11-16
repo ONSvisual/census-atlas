11913b84d75da64a242241a849e945896dff1d85
Valentina <vmilitaru28@gmail.com>
2021-11-16 09:38:20 +0000

    adds the ONSError component as a wrapper for the ONSTextField component

--- src/ui/ons/ONSTextField.svelte
+++ src/ui/ons/ONSTextField.svelte
@@ -1,42 +1,16 @@
 <script>
-  export let id, type, textFieldValue, onChange;
+  import ONSError from "./partials/ONSError.svelte";
+  export let id, type, textFieldValue, onChange, renderErrorMsg;
   export let hint = "";
   export let inputLabel = "";
   export let showPlaceholder = false;
   export let errorMessage = "Custom error message";
-  export let renderErrorMsg = false;
-
   function typeAction(node) {
     node.type = type;
   }
 </script>
 
-{#if renderErrorMsg}
-  <div class="ons-panel ons-panel--error ons-panel--no-title ons-u-mb-s" id="{id}-error">
-    <span class="ons-u-vh">Error: </span>
-    <div class="ons-panel__body">
-      <p class="ons-panel__error">
-        <strong>{errorMessage}</strong>
-      </p>
-      <div class="ons-field">
-        <label class="ons-label {showPlaceholder ? 'ons-label--placeholder' : ''}" for={id}>{inputLabel}</label>
-        {#if hint}
-          <span id="description-hint" class="ons-label__description  ons-input--with-description">
-            {hint}
-          </span>
-        {/if}
-        <input
-          {id}
-          class="ons-input ons-input--text ons-input-type__input {showPlaceholder ? 'ons-input--placeholder' : ''}"
-          placeholder={showPlaceholder ? inputLabel : ""}
-          bind:value={textFieldValue}
-          on:input={() => onChange(textFieldValue)}
-          use:typeAction
-        />
-      </div>
-    </div>
-  </div>
-{:else}
+<ONSError message={errorMessage} {id} {renderErrorMsg}>
   <div class="ons-field">
     <label class="ons-label {showPlaceholder ? 'ons-label--placeholder' : ''}" for={id}>{inputLabel}</label>
     {#if hint}
@@ -53,4 +27,4 @@
       use:typeAction
     />
   </div>
-{/if}
+</ONSError>
---
+++ src/ui/ons/partials/ONSError.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let message, id;
+  export let renderErrorMsg = false;
+</script>
+
+{#if renderErrorMsg}
+  <div class="ons-panel ons-panel--error ons-panel--no-title ons-u-mb-s" id="{id}-error">
+    <span class="ons-u-vh">Error: </span>
+    <div class="ons-panel__body">
+      <p class="ons-panel__error">
+        <strong>{message}</strong>
+      </p>
+      <slot />
+    </div>
+  </div>
+{:else}
+  <slot />
+{/if}
