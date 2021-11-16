6de42ac291c90c8b26b00888953d11e723e5930c
Valentina <vmilitaru28@gmail.com>
2021-11-16 14:29:30 +0000

    adds an inverted bool props which can toggle the component colour from black to white

--- src/ui/ons/ONSBacklink.svelte
+++ src/ui/ons/ONSBacklink.svelte
@@ -1,17 +1,19 @@
 <script>
   export let href = "/";
+  export let inverted = false;
+  let renderWhite = inverted ? "ons-breadcrumb__link--white" : "";
 </script>
 
 <nav class="ons-breadcrumb" aria-label="Back">
   <ol class="ons-breadcrumb__items ons-u-fs-s">
     <li class="ons-breadcrumb__item">
-      <a class="ons-breadcrumb__link" {href} id="back" data-attribute="Example attribute">Back</a>
+      <a class="ons-breadcrumb__link {renderWhite}" {href} id="back" data-attribute="Example attribute">Back</a>
       <svg
         class="ons-svg-icon "
         viewBox="0 0 8 13"
         xmlns="http://www.w3.org/2000/svg"
         focusable="false"
-        fill="currentColor"
+        fill={inverted ? "#fff" : "currentColor"}
       >
         <path
           d="M5.74,14.28l-.57-.56a.5.5,0,0,1,0-.71h0l5-5-5-5a.5.5,0,0,1,0-.71h0l.57-.56a.5.5,0,0,1,.71,0h0l5.93,5.93a.5.5,0,0,1,0,.7L6.45,14.28a.5.5,0,0,1-.71,0Z"
@@ -21,3 +23,9 @@
     </li>
   </ol>
 </nav>
+
+<style>
+  .ons-breadcrumb__link--white {
+    color: #fff;
+  }
+</style>
