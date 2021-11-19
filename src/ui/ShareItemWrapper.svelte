947f7d12e100d69ffc7a24c09a64dd24cd948c68
Valentina <vmilitaru28@gmail.com>
2021-11-19 14:03:22 +0000

    adds a share item wrapper component to align the icons like in the census atlas wireframe

---
+++ src/ui/ShareItemWrapper.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let censusAtlasSharePage;
+</script>
+
+{#if censusAtlasSharePage}
+  <div class="ons-grid__col ons-col-6@m ">
+    <div class="ons-pl-grid-col">
+      <slot />
+    </div>
+  </div>
+{:else}
+  <slot />
+{/if}
