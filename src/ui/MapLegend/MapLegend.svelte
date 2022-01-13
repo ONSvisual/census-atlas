da2a2e1433e18e85910e32776d1e67b9f08918f0
Michał Ordon <michal@tehidea.com>
2022-01-13 07:01:26 +0000

    Refactor MapLegend

--- src/ui/MapLegend/MapLegend.svelte
+++ src/ui/MapLegend/MapLegend.svelte
@@ -1,72 +1,97 @@
 <script>
-  import { LayerCake, Svg, flatten } from "layercake";
-  import { stack } from "d3-shape";
-  import { scaleBand, scaleOrdinal } from "d3-scale";
-
   import config from "../../config";
-  import Bar from "./Bar.svelte";
+  const colours = config.ux.legend_colours;
 
-  const data = [{ foo: 12, bar: 15, baz: 33 }];
-  const colours = config.ux.legend_colours.reverse();
+  export let value;
+  export let breaks;
+  export let average;
 
-  const xKey = [0, 1];
-  const yKey = "year";
-  const zKey = "key";
-
-  const seriesNames = Object.keys(data[0]).filter((d) => d !== yKey);
-
-  data.forEach((d) => {
-    seriesNames.forEach((name) => {
-      d[name] = +d[name];
-    });
-  });
-
-  const stackData = stack().keys(seriesNames);
-
-  const series = stackData(data);
+  const pos = (val) => {
+    let i = 0;
+    while (val > breaks[i + 1]) i += 1;
+    let unit = 100 / (breaks.length - 1);
+    let offset = (val - breaks[i]) / (breaks[i + 1] - breaks[i]);
+    return (i + offset) * unit;
+  };
 </script>
 
-<div class="chart-container">
-  <span class="category-percentage" />
-  <LayerCake
-    padding={{ top: 0, bottom: 30, left: 0 }}
-    x={xKey}
-    y={(d) => d.data[yKey]}
-    z={zKey}
-    yScale={scaleBand().paddingInner([0.1]).round(true)}
-    zScale={scaleOrdinal()}
-    zDomain={seriesNames}
-    zRange={colours}
-    flatData={flatten(series)}
-    data={series}
-  >
-    <Svg>
-      <Bar />
-      <text x="0" y="100%" class="lowest">Lowest</text>
-      <text x="50%" y="100%" text-anchor="middle" class="average">Average</text>
-      <text x="100%" y="100%" text-anchor="end" class="highest">Highest</text>
-    </Svg>
-  </LayerCake>
+<div class="maplegend">
+  {#each breaks.slice(1) as brk, i}
+    <div
+      class="block"
+      style="width: {100 / (breaks.length - 1)}%; left: {i * (100 / (breaks.length - 1))}%; background-color: {colours[
+        i
+      ]};"
+    />
+    <!-- <div class="line" style="left: {i * (100 / (breaks.length - 1))}%;" />
+    <div class="tick" style="left: {i * (100 / (breaks.length - 1))}%;">{breaks[i]}</div> -->
+  {/each}
+  <!-- <div class="line" style="left: 100%;" />
+  <div class="tick" style="left: 100%;">{breaks[breaks.length - 1]}</div> -->
+  <div class="average-marker" style="left: calc({pos(average)}% - 10px);" />
+  <div class="value">{value}%</div>
+</div>
+
+<div class="labels">
+  <div class="label-lowest">Lowest</div>
+  <div class="label-average">Average <span class="average-value">{average}%</span></div>
+  <div class="label-highest">Highest</div>
 </div>
 
 <style>
-  .chart-container {
+  .maplegend {
+    margin: 40px 0 4px;
+    box-sizing: border-box;
+    position: relative;
+    height: 20px;
     width: 100%;
+  }
+  .block {
+    position: absolute;
+    top: 0;
     height: 100%;
   }
-  .lowest {
+  .line {
     position: absolute;
-    right: 0;
-    bottom: 0;
+    top: 0;
+    height: 100%;
   }
-  .heavy {
-    font: bold 30px sans-serif;
+  .tick {
+    position: absolute;
+    z-index: 1;
+    top: 100%;
+    text-align: center;
+    transform: translateX(-50%);
   }
-
-  /* Note that the color of the text is set with the    *
-         * fill property, the color property is for HTML only */
-  .Rrrrr {
-    font: italic 40px serif;
-    fill: red;
+  .average-marker {
+    position: absolute;
+    width: 20px;
+    border-radius: 10px;
+    z-index: 2;
+    height: 100%;
+    border: 2px solid #fff;
+    background-color: #000;
+  }
+  .value {
+    position: absolute;
+    top: -40px;
+    font-size: 24px;
+    font-weight: bold;
+  }
+  .labels {
+    display: flex;
+    font-size: 14px;
+  }
+  .labels > div {
+    flex: 1;
+  }
+  .label-average {
+    text-align: center;
+  }
+  .label-average .average-value {
+    font-weight: bold;
+  }
+  .label-highest {
+    text-align: right;
   }
 </style>
