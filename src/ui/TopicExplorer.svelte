2a8c2da69cce484f7a7bd1fd7760fd4bbe266af3
Valentina <vmilitaru28@gmail.com>
2022-01-20 17:12:12 +0000

    adds the collapsible custom component

--- src/ui/TopicExplorer.svelte
+++ src/ui/TopicExplorer.svelte
@@ -1,7 +1,7 @@
 <script>
   import ONSAccordion from "./../ui/ons/ONSAccordion.svelte";
   import ONSAccordionPanel from "./../ui/ons/partials/ONSAccordionPanel.svelte";
-  import OnsCollapsible from "./ons/ONSCollapsible.svelte";
+  import CustomCollapsible from "./CustomCollapsible/CustomCollapsible.svelte";
   import censusData from "./../data/simpleTopicTableCategoryData";
   import { onMount } from "svelte";
   import slugify from "slugify";
@@ -36,7 +36,7 @@
   {#each censusData as topic, i}
     <ONSAccordionPanel id="topic-{i}" title={topic.name} noTopBorder description={topic.code}>
       {#each topic.tables as tableEntry}
-        <div>
+        <div class="table-margin--2">
           <h3 class="ons-related-links__title ons-u-fs-r--b ons-u-mb-xs">
             <a
               href="/{slugify(topic.name).toLowerCase()}/{slugify(tableEntry.name).toLowerCase()}/{slugify(
@@ -45,24 +45,23 @@
             >
           </h3>
           <p class="ons-collapsible__table-description">nomis table description - {tableEntry.code}</p>
+          <CustomCollapsible title={tableEntry.name}>
+            <ul class="ons-list ons-list--bare">
+              {#each tableEntry.categories as category}
+                <li class="ons-list__item">
+                  <a
+                    href="/{slugify(topic.name).toLowerCase()}/{slugify(tableEntry.name).toLowerCase()}/{slugify(
+                      category.name,
+                    ).toLowerCase()}{locationQueryParam}"
+                    class="ons-list__link">{category.name}</a
+                  >
+                </li>
+              {/each}
+            </ul>
+          </CustomCollapsible>
         </div>
-        <OnsCollapsible title={`Show ${tableEntry.name} options`}>
-          <ul class="ons-list ons-list--bare">
-            {#each tableEntry.categories as category}
-              <li class="ons-list__item">
-                <a
-                  href="/{slugify(topic.name).toLowerCase()}/{slugify(tableEntry.name).toLowerCase()}/{slugify(
-                    category.name,
-                  ).toLowerCase()}{locationQueryParam}"
-                  class="ons-list__link">{category.name}</a
-                >
-              </li>
-            {/each}
-          </ul>
-        </OnsCollapsible>
       {/each}
     </ONSAccordionPanel>
-    <!-- <OnsCollapsible title={"test test"}/> -->
   {/each}
 </ONSAccordion>
 
@@ -70,5 +69,8 @@
   @import "../../node_modules/@ons/design-system/scss/vars/_index.scss";
   .ons-collapsible__table-description {
     margin: 0 2rem 0 0;
+  }
+  .table-margin--2 {
+    margin: 1rem 0 2rem 0;
   }
 </style>
