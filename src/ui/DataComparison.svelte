62c8c7ec7d7268de8f3e3b5870ab0886f539a3b3
Patrick Fleming <patrickjosephfleming@gmail.com>
2022-01-21 12:20:42 +0000

    completes styling

--- src/ui/DataComparison.svelte
+++ src/ui/DataComparison.svelte
@@ -4,25 +4,36 @@
 
   function populateComparisonString(difference) {
     if (difference > 0) {
-      return `${difference.toString()}% higher`;
+      return {
+        percentage: difference.toString(),
+        comparative: "% higher"
+      };
     } else if (difference < 0) {
-      return `${difference.toString()}% lower`;
+      return {
+        percentage: Math.abs(difference).toString(),
+        comparative: "% lower"
+      };;
     }
-    return "The same as";
+    return {
+      percentage: null,
+      comparative: "The same as",
+    };
   }
 
-  const comparisonString = populateComparisonString(difference);
+  const comparisonObj = populateComparisonString(difference);
 </script>
 
 <div class="data-comparison-container">
   <div class="data-display-panel">
-      <div class="ons-grid--flex ons-grid--between ons-grid--vertical-center">
-        <span>
-            <svg fill="#ffffff" class="data-comparison-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-              <path d="M24 22h-24l12-20z" />
-            </svg>
-            <p class="data-comparison-graphic-text">{comparisonString}</p>
-          </span>
+      <div class="ons-grid--flex ons-grid--align-mid">
+        <div class="data-comparison-icon">
+          <svg fill="#ffffff" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+            <path d="M24 22h-24l12-20z" />
+          </svg>
+        </div>
+
+            <p class="ons-u-fs-xl data-comparison-graphic-text">{comparisonObj.percentage}</p><p class="ons-u-fs-m data-comparison-graphic-text
+            data-comparison-graphic-text--small">{comparisonObj.comparative}</p>
       </div>
   </div>
 
@@ -43,6 +54,7 @@
     position: relative;
     width: 100%;
     height: 50%;
+    padding: 0.4rem 1rem;
   }
 
   .data-display-panel {
@@ -57,8 +69,29 @@
     display: inline-block;
   }
 
+  .data-comparison-icon {
+    margin: 0 .7rem .35rem 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+  }
+
   .data-comparison-graphic-text {
       color: $color-white;
       font-weight: bold;
+      margin: 0;
+
+      &--small {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-end;
+        margin: 0 0 .1rem 0;
+      }
+  }
+
+  @media only screen and (max-width: map-get($grid-bp, m)) {
+    .data-comparison-graphic-text--small {
+      margin: 0 0 .15rem 0;
+    }
   }
 </style>
