e7439fa433bbb7ea7952cafe1e0f2e4a745a7bec
Michał Ordon <michal@tehidea.com>
2022-01-26 09:48:03 +0000

    Cleanup

--- src/ui/CategorySelector.svelte
+++ src/ui/CategorySelector.svelte
@@ -1,29 +1,7 @@
 <script>
   export let topicSlug, tableSlug, locationId;
-  export let categories = [
-    {
-      code: "QS302EW001",
-      name: "Very good health",
-    },
-    {
-      code: "QS302EW002",
-      name: "Good health",
-    },
-    {
-      code: "QS302EW003",
-      name: "Fair health",
-    },
-    {
-      code: "QS302EW004",
-      name: "Bad health",
-    },
-    {
-      code: "QS302EW005",
-      name: "Very bad health",
-    },
-  ];
-
-  export let selectedCategory = "QS302EW005";
+  export let categories = [];
+  export let selectedCategory = "";
 
   $: locationQueryParam = locationId ? `?location=${locationId}` : "";
   let leftIndex, rightIndex, selectedCatIndex;
@@ -63,12 +41,12 @@
   }
 </script>
 
-<div class="category-selector ons-grid--flex ons-grid--vertical-center">
-  <div class="ons-grid__col ons-col-4@m selector-col ">
-    <div class="ons-pl-grid-col ons-grid--flex ons-grid__col--flex" style="justify-content:left">
+<div class="category-selector">
+  <div class="">
+    <div class="">
       <span>&#60;&#160;</span>
       <a
-        class="category-selector__button cut-text"
+        class="category-selector__button"
         href="/{topicSlug}/{tableSlug}/{categories[selectedCatIndex].slug}{locationQueryParam}"
         on:click={clickLeft}
       >
@@ -76,15 +54,10 @@
       </a>
     </div>
   </div>
-  <div class="ons-grid__col ons-col-4@m selector-col ons-grid--flex">
-    <div class="ons-pl-grid-col">
-      <p class="cut-text">{categories[selectedCatIndex].name}</p>
-    </div>
-  </div>
-  <div class="ons-grid__col ons-col-4@m selector-col">
-    <div class="ons-pl-grid-col ons-grid--flex" style="justify-content:right">
+  <div class="">
+    <div class="">
       <a
-        class="category-selector__button category-selector__button__left cut-text"
+        class="category-selector__button"
         href="/{topicSlug}/{tableSlug}/{categories[selectedCatIndex].slug}{locationQueryParam}"
         on:click={clickRight}
       >
@@ -96,56 +69,4 @@
 </div>
 
 <style>
-  .cut-text {
-    overflow: hidden;
-    text-overflow: ellipsis;
-    display: -webkit-box;
-    -webkit-line-clamp: 3;
-    -webkit-box-orient: vertical;
-  }
-  .category-selector {
-    background-color: #003c57;
-    color: #fff;
-    padding: 10px 15px 10px;
-  }
-  .category-selector__button {
-    color: #fff;
-    font-size: 1rem;
-    font-weight: normal;
-  }
-
-  .category-selector__button__left {
-    text-align: right;
-  }
-  @media (max-width: 400px) {
-    .category-selector__button,
-    span {
-      font-size: 0.8em;
-    }
-  }
-  .category-selector__button {
-    text-decoration: underline;
-    text-underline-position: unset;
-  }
-  .selector-col {
-    max-width: 33.33333%;
-    width: 33.33333%;
-    padding-left: 0;
-  }
-  p {
-    text-align: center;
-    margin-bottom: 0;
-    font-size: 1.3em;
-    font-weight: bold;
-  }
-  .ons-grid--flex {
-    flex-wrap: nowrap;
-    align-items: center;
-  }
-
-  @media (max-width: 400px) {
-    p {
-      font-size: 1em;
-    }
-  }
 </style>
