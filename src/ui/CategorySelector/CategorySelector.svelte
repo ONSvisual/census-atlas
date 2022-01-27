82076a9729190870b5aa76683c25c1068aa973ab
Michał Ordon <michal@tehidea.com>
2022-01-27 23:30:16 +0000

    Redesign CategorySelector

--- src/ui/CategorySelector/CategorySelector.svelte
+++ src/ui/CategorySelector/CategorySelector.svelte
@@ -42,31 +42,88 @@
 </script>
 
 <div class="category-selector">
-  <div class="">
-    <div class="">
-      <span>&#60;&#160;</span>
-      <a
-        class="category-selector__button"
-        href="/{topicSlug}/{tableSlug}/{categories[selectedCatIndex].slug}{locationQueryParam}"
-        on:click={clickLeft}
-      >
-        {categories[leftIndex].name}
-      </a>
+  <div class="ons-grid ons-grid--gutterless ons-grid--vertical-center">
+    <div class="ons-grid__col ons-col-9@xxs">
+      <div class="category-selector__category">
+        {categories[selectedCatIndex].name}
+      </div>
+    </div>
+    <div class="ons-grid__col ons-col-3@xxs">
+      <div class="category-selector__stats">%</div>
     </div>
   </div>
-  <div class="">
-    <div class="">
-      <a
-        class="category-selector__button"
-        href="/{topicSlug}/{tableSlug}/{categories[selectedCatIndex].slug}{locationQueryParam}"
-        on:click={clickRight}
-      >
-        {categories[rightIndex].name}
-      </a>
-      <span>&#160;&#62;</span>
-    </div>
+  <div class="category-selector__links">
+    <a
+      class="category-selector__link category-selector__link--previous"
+      href="/{topicSlug}/{tableSlug}/{categories[selectedCatIndex].slug}{locationQueryParam}"
+      on:click={clickLeft}
+    >
+      <span>{categories[leftIndex].name}</span>
+    </a>
+
+    <a
+      class="category-selector__link category-selector__link--next"
+      href="/{topicSlug}/{tableSlug}/{categories[selectedCatIndex].slug}{locationQueryParam}"
+      on:click={clickRight}
+    >
+      <span>{categories[rightIndex].name}</span>
+    </a>
   </div>
 </div>
 
-<style>
+<style lang="scss">
+  @import "../../node_modules/@ons/design-system/scss/vars/_index.scss";
+
+  .category-selector {
+    background: #003c57;
+    margin: -18px -18px 0;
+    padding: 18px;
+    color: $color-white;
+
+    &__category {
+      font-size: 16px;
+      line-height: 20px;
+      font-weight: 700;
+      padding-bottom: 18px;
+      @media (min-width: map-get($grid-bp, s)) {
+        font-size: 20px;
+        line-height: 25px;
+      }
+    }
+
+    &__links {
+      display: flex;
+      align-items: stretch;
+    }
+
+    &__link {
+      display: flex;
+      width: 50%;
+      font-size: 13px;
+      line-height: 18px;
+      @media (min-width: map-get($grid-bp, s)) {
+        font-size: 16px;
+        line-height: 22px;
+      }
+      span {
+        color: $color-white;
+        padding: 0 16px 0 16px;
+        align-self: center;
+        justify-self: end;
+      }
+      &--previous {
+        & span {
+          background: url(./chevron--left.svg) no-repeat 0 50%;
+        }
+      }
+      &--next {
+        text-align: right;
+        justify-content: end;
+        border-left: 1px solid rgba($color-white, 0.5);
+        & span {
+          background: url(./chevron--right.svg) no-repeat 100% 50%;
+        }
+      }
+    }
+  }
 </style>
