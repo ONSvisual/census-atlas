4bfa6ccd32687049acb5f0c0470877ff0e3cc032
Michał Ordon <michal@tehidea.com>
2022-01-28 11:45:03 +0000

    Delete dev navigation

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -18,46 +18,6 @@
 
 <slot />
 
-<nav
-  style="position: relative; z-index: 99999; background: #e2e2e3; padding: 20px"
-  class="ons-related-links__navigation"
-  aria-labelledby="related-links"
->
-  <ul class="ons-list ons-list--bare">
-    <li class="ons-list__item">
-      <a href="/components">Components</a>
-    </li>
-    <li class="ons-list__item">
-      <a href="/design-system">Design System</a>
-    </li>
-    <li class="ons-list__item">
-      <a href="/">Explore Census</a>
-    </li>
-    <li class="ons-list__item">
-      <a href="/health">Explore by Topic (Health)</a>
-    </li>
-    <li class="ons-list__item">
-      <a href="/health/general-health/good-health">Explore by Category (General health → Good health)</a>
-    </li>
-    <!--
-    <li class="ons-list__item">
-      <a href="/locations/Walsall">Explore by Location (Walsall)</a>
-    </li>
-    <li class="ons-list__item">
-      <a href="/General health/locations/England & Wales">Explore by Category & Location</a>
-    </li>
-    -->
-    <li class="ons-list__item">
-      <a href="/categories">Explore Census Categories</a>
-    </li>
-    <!--
-    <li class="ons-list__item">
-      <a href="/original">Original census atlas version</a>
-    </li>
-    -->
-  </ul>
-</nav>
-
 <style lang="scss" global>
   /* XXX: Replicate main.scss in order to work around some bugs on upstream lulz */
   @import "normalize";
