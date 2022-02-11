a9efb9eef6ffa11782f7b72f6fd7c4353dcac29d
Valentina <vmilitaru28@gmail.com>
2022-02-11 17:15:30 +0000

    refactores the setMapGeographyColours test

--- src/ui/map/dataLayer.test.js
+++ src/ui/map/dataLayer.test.js
@@ -14,18 +14,20 @@ import { selectedCategoryBreaks } from "../../model/metadata/metadata";
 import { dataByGeography, cachedMapCategories, newDataByGeography } from "../../model/censusdata/censusdata";
 
 describe("DataLayer", () => {
-  beforeEach(() => {
-    dataByGeography.set(new Map([["geoCode1", new Map([["catCode1", { value: 1, perc: 20, total: 5 }]])]]));
-    selectedCategoryBreaks.set({
-      lad: [10, 20, 30, 45, 50],
-      lsoa: [9, 15, 17, 26, 34],
-    });
-    newDataByGeography.set(true);
-    cachedMapCategories.set(new Set(["catCode1"]));
+  selectedCategoryBreaks.set({
+    lad: [10, 20, 30, 45, 50],
+    lsoa: [9, 15, 17, 26, 34],
   });
-
-  it("setMapGeographyColours", async () => {
+  newDataByGeography.set(true);
+  cachedMapCategories.set(new Set(["catCode1", "catCode2"]));
+  it("setMapGeographyColours - returns the colour index of the LAD geography", () => {
+    dataByGeography.set(new Map([["LADgeoCode", new Map([["catCode1", { value: 1, perc: 20, total: 5 }]])]]));
     const { component } = render(MockMap, { props: { Component: DataLayer, catCode: "catCode1" } });
     expect(component.legendSection).toEqual(1);
+  });
+  it("setMapGeographyColours - returns the colour index of the LSOA geography", () => {
+    dataByGeography.set(new Map([["E01LSOAgeoCode", new Map([["catCode2", { value: 5, perc: 50, total: 10 }]])]]));
+    const { component } = render(MockMap, { props: { Component: DataLayer, catCode: "catCode2" } });
+    expect(component.legendSection).toEqual(4);
   });
 });
