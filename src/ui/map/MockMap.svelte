6635a02f2e86ac69cfc11aaeb31653bcdd83d58e
Valentina <vmilitaru28@gmail.com>
2022-02-11 13:02:26 +0000

    fixes the tests for the setMapGeographyColours function

--- src/ui/map/MockMap.svelte
+++ src/ui/map/MockMap.svelte
@@ -2,11 +2,11 @@
   import { Map } from "mapbox-gl";
   import { setContext } from "svelte";
 
-  export let Component;
+  export let Component, catCode, setMapGeographyColours, legendSection;
 
   setContext("map", {
     getMap: () => new Map({}),
   });
 </script>
 
-<svelte:component this={Component} />
+<div><Component {catCode} bind:setMapGeographyColours bind:legendSection /></div>
--- src/ui/map/dataLayer.test.js
+++ src/ui/map/dataLayer.test.js
@@ -3,31 +3,29 @@ jest.mock("mapbox-gl", () => ({
     getLayer: jest.fn(),
     removeLayer: jest.fn(),
     addLayer: jest.fn(),
+    setFeatureState: jest.fn(),
   })),
-}));
-
-jest.mock("../../model/metadata/metadata", () => ({
-  __esModule: true,
-  $selectedCategoryBreaks: {
-    lad: [10, 20, 30, 45, 50],
-    lsoa: [9, 15, 17, 26, 34],
-  },
-}));
-jest.mock("../../model/censusdata/censusdata", () => ({
-  __esModule: true,
-  $newDataByGeography: true,
-  $cachedMapCategories: new Set(["catCode1"]),
-  $dataByGeography: new Map([["geoCode1", new Map([["catCode1", { value: 1, perc: 20, total: 5 }]])]]),
 }));
 
 import { render } from "@testing-library/svelte";
 import DataLayer from "./DataLayer.svelte";
 import MockMap from "./MockMap.svelte";
+import { selectedCategoryBreaks } from "../../model/metadata/metadata";
+import { dataByGeography, cachedMapCategories, newDataByGeography } from "../../model/censusdata/censusdata";
 
 describe("DataLayer", () => {
+  beforeEach(() => {
+    dataByGeography.set(new Map([["geoCode1", new Map([["catCode1", { value: 1, perc: 20, total: 5 }]])]]));
+    selectedCategoryBreaks.set({
+      lad: [10, 20, 30, 45, 50],
+      lsoa: [9, 15, 17, 26, 34],
+    });
+    newDataByGeography.set(true);
+    cachedMapCategories.set(new Set(["catCode1"]));
+  });
+
   it("setMapGeographyColours", async () => {
-    const component = render(MockMap, { props: { Component: DataLayer } });
-    expect(component.setMapGeographyColours()).toEqual(1);
+    const { component } = render(MockMap, { props: { Component: DataLayer, catCode: "catCode1" } });
     expect(component.legendSection).toEqual(1);
   });
 });
