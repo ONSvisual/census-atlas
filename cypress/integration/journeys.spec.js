cb53cbdb45035e5a01a457271e16733915a66a1c
Michał Ordon <michal@tehidea.com>
2022-02-11 19:15:12 +0000

    Add Route 2

--- cypress/integration/journeys.spec.js
+++ cypress/integration/journeys.spec.js
@@ -1,10 +1,8 @@
-// journeys.spec.js created with Cypress
-
 beforeEach(() => {
   cy.visit("http://localhost:3000");
 });
 
-describe("Route 1", () => {
+describe("User Testing Scenarios", () => {
   it("can go through Route 1", () => {
     cy.contains("Choose a category").click();
     cy.url().should("include", "/categories");
@@ -18,5 +16,16 @@ describe("Route 1", () => {
     cy.contains("Show Heating options").click();
     cy.contains("Gas").click();
     cy.url().should("include", "/housing/heating/gas");
+    cy.get(".mapboxgl-canvas").should("be.visible");
+  });
+  it("can go through Route 2", () => {
+    cy.get("#search-field").type("lon").type("{downarrow}").type("{enter}");
+    cy.contains("Choose").click();
+    cy.url().should("include", "/categories");
+    cy.contains("Work").click();
+    cy.contains("Show Working hours options").click();
+    cy.contains("Part-time (less than 30 hours per week)").click();
+    cy.url().should("include", "/work/working-hours/part-time-less-than-30-hours-per-week");
+    cy.get(".mapboxgl-canvas").should("be.visible");
   });
 });
