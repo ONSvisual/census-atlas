cea06f91d8fb4f58b8235621bd075cc297856c48
Valentina <vmilitaru28@gmail.com>
2022-02-14 14:41:56 +0000

    fixes tests for writeCsvDataToMapObj function and adds new tests for totalCodesRequested function

--- src/model/utils.test.js
+++ src/model/utils.test.js
@@ -1,4 +1,4 @@
-import { getLegendSection, writeCsvDataToMapObj } from "./utils";
+import { getLegendSection, writeCsvDataToMapObj, totalCodesRequested } from "./utils";
 import { totalCatCodeLookup, reverseTotalCatCodeLookup } from "../model/metadata/metadata";
 
 describe("getLegendSection", () => {
@@ -34,34 +34,85 @@ describe("getLegendSection", () => {
 
 describe("writeCsvDataToMapObj function", () => {
   const geoCode = "geoCode1";
-  totalCatCodeLookup.set({ catCode: "totalCode" });
-  reverseTotalCatCodeLookup.set({ totalCode: "catCode" });
+  totalCatCodeLookup.set({ catCode1: "totalCode1", catCode2: "totalCode1" });
+  reverseTotalCatCodeLookup.set({
+    totalCode1: [
+      { code: "catCode1", name: "Cat code 1", slug: "cat-code-1" },
+      { code: "catCode2", name: "Cat code 2", slug: "cat-code-2" },
+    ],
+  });
   const error = jest.spyOn(console, "error").mockImplementation(() => {});
+  afterEach(() => {
+    error.mockReset();
+  });
   it("returns correctly structured data given correct inputs and store values", () => {
-    const responseStr = `geography_code,catCode,totalCode\ngeoCode1,100,200`;
+    const responseStr = `geography_code,catCode1,totalCode1\ngeoCode1,100,200`;
     const data = writeCsvDataToMapObj(responseStr, geoCode);
-    const expectedData = new Map([["geoCode1", new Map([["catCode", { value: 100, total: 200, perc: 50 }]])]]);
+    const expectedData = new Map([["geoCode1", new Map([["catCode1", { value: 100, total: 200, perc: 50 }]])]]);
 
     expect(data).toEqual(expectedData);
     //check geography_code column in CSV response is not written to Map
     expect(data.has("geography_code")).toBe(false);
     //check total code is not written to inner Map
-    expect(data.get("geoCode1").has("totalCode")).toBe(false);
+    expect(data.get("geoCode1").has("totalCode1")).toBe(false);
   });
   it("returns missing total category error if total category data is not in response string", () => {
-    const responseStr = `geography_code,catCode\ngeoCode1,100`;
+    const responseStr = `geography_code,catCode1\ngeoCode1,100`;
     writeCsvDataToMapObj(responseStr, geoCode);
 
     expect(error).toBeCalledWith("Total category data not found in API response.");
-    error.mockReset();
   });
   it("returns missingCatCodes error if data in response is not in front end metadata structure", () => {
-    const responseStr = `geography_code,catCode2,totalCode\ngeoCode1,100,200`;
+    const responseStr = `geography_code,catCode3,totalCode1\ngeoCode1,100,200`;
     writeCsvDataToMapObj(responseStr, geoCode);
 
     expect(error).toBeCalledWith(
-      "The following category codes were received from the API but are not available in the current metadata - catCode2.",
+      "The following category codes were received from the API but are not available in the current metadata - catCode3.",
     );
+  });
+});
+
+describe("totalCodesRequested function", () => {
+  const error = jest.spyOn(console, "error").mockImplementation(() => {});
+  totalCatCodeLookup.set({ catCode1: "totalCode1", catCode2: "totalCode1" });
+  reverseTotalCatCodeLookup.set({
+    totalCode1: [
+      { code: "catCode1", name: "Cat code 1", slug: "cat-code-1" },
+      { code: "catCode2", name: "Cat code 2", slug: "cat-code-2" },
+    ],
+    totalCode2: [{ code: "catCode3", name: "Cat code 3", slug: "cat-code-3" }],
+  });
+  let requestedCategories = ["totalCode1", "catCode1"];
+  afterEach(() => {
     error.mockReset();
+  });
+  it("returns true and no errors when only related totals and categories are requested", () => {
+    expect(totalCodesRequested(requestedCategories)).toEqual(true);
+  });
+  it("returns false with correct error when a category code is passed without the related total code", () => {
+    let requestedCategories = ["catCode1"];
+    expect(totalCodesRequested(requestedCategories)).toEqual(false);
+    expect(error).toBeCalledWith(
+      "The following category codes were requested without requesting a corresponding total code: catCode1",
+    );
+  });
+  it("returns false with correct error when a total code is passed without the related category code", () => {
+    let requestedCategories = ["totalCode1"];
+    expect(totalCodesRequested(requestedCategories)).toEqual(false);
+    expect(error).toBeCalledWith(
+      "The following total codes were requested without requesting a corresponding category code: totalCode1",
+    );
+  });
+  it("returns false with correct errors when a total code is passed with an unrelated category code", () => {
+    let requestedCategories = ["totalCode2", "catCode1"];
+    expect(totalCodesRequested(requestedCategories)).toEqual(false);
+    expect(error).toHaveBeenNthCalledWith(
+      1,
+      "The following category codes were requested without requesting a corresponding total code: catCode1",
+    );
+    expect(error).toHaveBeenNthCalledWith(
+      2,
+      "The following total codes were requested without requesting a corresponding category code: totalCode2",
+    );
   });
 });
