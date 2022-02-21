1f7fff85e9f61604083164c1d6868dc7cfde7d9e
Michał Ordon <michal@tehidea.com>
2022-02-21 12:26:15 +0000

    Add delays to account for slow internets

--- cypress/integration/journeys.spec.js
+++ cypress/integration/journeys.spec.js
@@ -1,10 +1,12 @@
 beforeEach(() => {
   cy.visit("http://localhost:3000");
+  cy.wait(500);
 });
 
 describe("User Testing Scenarios", () => {
   it("can go through Route 1", () => {
     cy.contains("Choose a category").click();
+    cy.wait(500);
     cy.url().should("include", "/categories");
     cy.contains("Education").should("be.visible");
     cy.contains("Health").should("be.visible");
@@ -15,16 +17,19 @@ describe("User Testing Scenarios", () => {
     cy.contains("Housing").click();
     cy.contains("Show Heating options").click();
     cy.contains("Gas").click();
+    cy.wait(500);
     cy.url().should("include", "/housing/heating/gas");
     cy.get(".mapboxgl-canvas").should("be.visible");
   });
   it("can go through Route 2", () => {
     cy.get("#search-field").type("lon").type("{downarrow}").type("{enter}");
     cy.contains("Choose").click();
+    cy.wait(500);
     cy.url().should("include", "/categories");
     cy.contains("Work").click();
     cy.contains("Show Working hours options").click();
     cy.contains("Part-time (less than 30 hours per week)").click();
+    cy.wait(500);
     cy.url().should("include", "/work/working-hours/part-time-less-than-30-hours-per-week");
     cy.get(".mapboxgl-canvas").should("be.visible");
   });
