d20b481a7793647b921bce0f150fd226fce7aecf
Patrick Fleming <patrickjosephfleming@gmail.com>
2022-03-09 10:52:02 +0000

    sets up component

---
+++ src/ui/ExploreSomethingElseNav/ExploreSomethingElseNav.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let firstLink,
+    secondLink = { text: null, url: null };
+</script>
+
+<div class="component-margin--2">
+  <h2 class="ons-u-fs-m ons-u-mb-xs">Explore something else</h2>
+  <p class="ons-u-mb-xs"><a href={firstLink.url}>{firstLink.text}</a></p>
+  {#if secondLink}
+    <p class="ons-u-mb-xs"><a href={secondLink.url}>{secondLink.text}</a></p>
+  {/if}
+  <p class="ons-u-mb-xs"><a href="/">Back to the start</a></p>
+</div>
+
+<style>
+  .component-margin--2 {
+    margin-bottom: 2rem;
+  }
+</style>
