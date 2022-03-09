dec2d6228feaa0041d448429b872e4bf219d93cd
James Stephens <james.stephens@nitrondevelopments.co.uk>
2022-03-09 09:34:49 +0000

    Feature: Init commit.

--- src/routes/area/index.svelte
+++ src/routes/area/index.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { afterUpdate, beforeUpdate, onMount } from "svelte";
   import BasePage from "../../ui/BasePage.svelte";
   import MapWrapper from "../../ui/map/MapWrapper.svelte";
   import Topic from "../../ui/Topic.svelte";
@@ -18,13 +19,50 @@
 
   import { goto } from "$app/navigation";
   import { page } from "$app/stores";
-  import { onMount } from "svelte";
+
+  import {
+    tables,
+    dataByGeography,
+    fetchSelectedDataForGeographies,
+    getCategoryBySlug,
+  } from "../../model/censusdata/censusdata";
+  import GeodataApiDataService from "../../model/censusdata/services/geodataApiDataService";
+  import config from "../../config";
 
   let locationName, locationId;
   let topicSuggestions;
+  let geoCode;
+  let allPeopleTotal = "";
+  let allHouseholdsTotal = "";
+  let loading = true;
 
   onMount(async () => {
     $pageUrl = $page.path;
+  });
+
+  /* Fix reactivity */
+  const fetchData = async (geo, codes) => {
+    await fetchSelectedDataForGeographies(new GeodataApiDataService(), geo, codes, config.stores.overwrite);
+    loading = false;
+  };
+
+  afterUpdate(async () => {
+    loading = true;
+    geoCode = $page.query.get("location");
+    const peopleCategory = getCategoryBySlug("sex", "female");
+    const peopleTable = peopleCategory ? tables[peopleCategory.table] : null;
+    const peopleCatCode = peopleTable?.total;
+    const housingCategory = getCategoryBySlug("size-of-household", "1-person-household");
+    const housingTable = housingCategory ? tables[housingCategory.table] : null;
+    const housingCatCode = housingTable?.total;
+    if (loading && peopleTable && housingTable) {
+      fetchData(geoCode, [...housingTable.categories, ...peopleTable.categories, housingCatCode, peopleCatCode]);
+    }
+    if ($dataByGeography.has(geoCode)) {
+      console.log($dataByGeography.has(geoCode));
+      allHouseholdsTotal = $dataByGeography.get(geoCode).get("QS406EW0002")["total"].toLocaleString();
+      allPeopleTotal = $dataByGeography.get(geoCode).get("QS104EW0002")["total"].toLocaleString();
+    }
   });
 
   function initialisePage() {
@@ -63,7 +101,21 @@
     <MapWrapper showDataLayer={false} />
   </span>
 
-  <Topic cardTitle="{locationName}'s Census" topicList={topicSuggestions}>
+  <Topic cardTitle="Explore {locationName}" topicList={topicSuggestions}>
+    <div class="ons-grid explore-container">
+      <div class="ons-grid__col ons-col-6@m">
+        <div class="ons-pl-grid-col">
+          <h3 class="ons-u-fs-s explore-sub-title">All people (usual residents)</h3>
+          <p class="ons-u-fs-l explore-data">{allPeopleTotal}</p>
+        </div>
+      </div>
+      <div class="ons-grid__col ons-col-6@m">
+        <div class="ons-pl-grid-col">
+          <h3 class="ons-u-fs-s explore-sub-title">All households</h3>
+          <p class="ons-u-fs-l explore-data">{allHouseholdsTotal}</p>
+        </div>
+      </div>
+    </div>
     <p>The 2021 Census tells us a lot about how people in {locationName} live and work.</p>
     <p>
       <a href="/categories{locationId ? `?location=${locationId}` : ''}">Choose a category from the full list</a> or explore
@@ -93,3 +145,15 @@
     </footer>
   </span>
 </BasePage>
+
+<style>
+  .explore-container {
+    margin-bottom: 1.6rem;
+  }
+  .explore-sub-title {
+    margin-bottom: unset;
+  }
+  .explore-data {
+    margin-bottom: unset;
+  }
+</style>
